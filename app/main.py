8e44c00d29986bd14f80e946ac81090bf06055c9
kellylop613 <johalopera15@gmail.com>
2026-09-23 21:54:53 -0500

    Implementar seguridad y autenticacion en FastAPI

--- app/main.py
+++ app/main.py
@@ -1,41 +1,56 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database.connection import Base, engine
+from app.middlewares.request_middleware import custom_request_middleware
 from app.models.user_model import User
 from app.models.device_model import Device
 from app.models.loan_model import Loan
 from app.routes.user_routes import router as user_router
 from app.routes.device_routes import router as device_router
 from app.routes.loan_routes import router as loan_router
+from app.auth.auth_routes import router as auth_router
+from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
+
+from app.rate_limiter import limiter
 
 app = FastAPI(
-    title="Device Systems API",
-    description=(
-        "API REST para la gestión de usuarios del sistema "
-        "device_systems. Incluye operaciones CRUD, validación "
-        "de datos, manejo de errores y Dependency Injection."
-    ),
-    version="2.0.0",
+    title="device_systems API",
+    description="API REST segura para gestión de usuarios, dispositivos y préstamos",
+    version="3.0.0",
     contact={
         "name": "Kelly Lopera",
         "email": "johalopera15@gmail.com"
     }
 )
 
+app.state.limiter = limiter
+app.add_exception_handler(
+    RateLimitExceeded,
+    _rate_limit_exceeded_handler
+)
 
+app.middleware("http")(custom_request_middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(device_router)
 app.include_router(loan_router)
+
 @app.get("/")
 def root():
     return {
         "message": "Device Systems API funcionando",
-        "version": "2.0.0"
+        "version": "3.0.0"
     }
-
-
-@app.middleware("http")
-async def add_custom_headers(request, call_next):
-    response = await call_next(request)
-    response.headers["X-App-Name"] = "device_systems"
-    response.headers["X-API-Version"] = "2.0.0"
-    return response
